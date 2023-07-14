cbd1bb6b35e8d3b5df1bc5970809dae912a284d7
Nourhan-selim <nourhanselim8@gmail.com>
2023-07-14 14:30:37 -0700

    a

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int i = 0;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i < 57; i++)
 	{
 		putchar(i);
 		putchar(',');
 		putchar(' ');
 	}
-
+	putchar(57);
 	putchar('\n');
 	return (0);
 }
