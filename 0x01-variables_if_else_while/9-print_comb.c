ae00a2083d7d8b63f3813e399f88b8d0e11786c9
Nourhan-selim <nourhanselim8@gmail.com>
2023-07-14 14:22:22 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(', ');
+	}
+
+	putchar('\n');
+	return (0);
+}
