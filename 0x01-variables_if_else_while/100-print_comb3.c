528ef669ed4b2e1c76a3e76a038f9d74f9826b08
Nourhan-selim <72805007+Nourhan-selim@users.noreply.github.com>
2023-07-16 12:34:00 +0000

    a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,16 +4,22 @@
  *
  * Return: 0
  */
-int main(void)
-{
-	int i ,j ;
+int main()
+{   
+	int i ,j;
+    for (i = 48 ;i <= 56 ;i++)
+    {   for (j=48 ;j <= 57; j++)
+        {   if (i != j && i < j)
+            {
+                putchar(i);
+                putchar(j);
+                if(i != 56 && i != 57)
+                {   putchar(',');
+                    putchar(' ');
+                }
+            }
 
-	for (i=0; i <= 8; i++)
-		if (i != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		i++;
-	putchar('\n');
-	return (0);
+        }
+    }
+    return 0;
+}
