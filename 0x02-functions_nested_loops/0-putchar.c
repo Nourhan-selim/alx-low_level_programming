5c451072fc65bfcca9274d607417ac48315376f8
Nourhan-selim <72805007+Nourhan-selim@users.noreply.github.com>
2023-07-19 11:08:07 +0000

    f

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*main - function prints text as output
+*
+*Return:return 0
+*/
+int main(void)
+{
+	char school[] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(\n);
+	return (0);
+}
