ca8b9485a42bd884806d8e65291c2c3920aa1aa7
Nourhan-selim <nourhanselim8@gmail.com>
2023-09-18 14:25:57 -0700

    a

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
+
 /**
-*print_numbers - function that print all numbers from
-*
-*Return: value of pointer
-*/
+ * reset_to_98 - updates the value of the variable
+ * the pointer points to to 98
+ * @n: pointer to the variable to update
+ */
 void reset_to_98(int *n)
 {
-	*n=98;
-	return (0);
+	*n = 98;
 }
